7661594b5cca00524d5bb3ac88f7484743451a19
Christopher Hazlett <chris@masonhub.co>
2020-09-17 12:29:58 -0400

    Fixed import of mercury

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/masonhubco/rebar
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/masonhubco/mercury v0.9.2
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/stretchr/testify v1.6.1
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- helpers/api_handler_test.go
+++ helpers/api_handler_test.go
@@ -5,7 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/masonhubco/mercury/rebar"
+	"github.com/masonhubco/rebar/helpers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -18,7 +18,7 @@ func Test_APIRender(t *testing.T) {
 			FieldTwo string `json:"FieldTwo"`
 		}
 
-		rebar.APIRenderList(rr, req, 1, Widgets{FieldOne: "one", FieldTwo: "two"})
+		helpers.APIRenderList(rr, req, 1, Widgets{FieldOne: "one", FieldTwo: "two"})
 		assert.Equal(t, http.StatusOK, rr.Code)
 		assert.Equal(t, `{"FieldOne":"one","FieldTwo":"two"}
 `, rr.Body.String())
@@ -32,7 +32,7 @@ func Test_APIRender(t *testing.T) {
 			FieldOne func()
 		}
 
-		rebar.APIRenderList(rr, req, 1, Widgets{FieldOne: func() {}})
+		helpers.APIRenderList(rr, req, 1, Widgets{FieldOne: func() {}})
 		assert.Equal(t, http.StatusInternalServerError, rr.Code)
 		assert.Equal(t, "1", rr.Header().Get("Result-Count"))
 		assert.Equal(t, "error rendering JSON\n", rr.Body.String())
