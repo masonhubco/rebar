a2a94f5b06c213a074a406d21a463036d0fefce4
Jawad Ali <jawad@masonhub.co>
2021-01-14 17:35:12 +0100

    Enabled playground only for development environment

--- samples/graphql/app/app.go
+++ samples/graphql/app/app.go
@@ -2,12 +2,10 @@ package app
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/gorilla/websocket"
 	"github.com/masonhubco/rebar/middleware"
 	"github.com/masonhubco/rebar/samples/graphql/api"
 	"github.com/masonhubco/rebar/samples/graphql/graph"
@@ -19,10 +17,11 @@ var app *service.Rebar
 
 func App() *service.Rebar {
 	if app == nil {
-		app = service.New(service.Options{
+		options := service.Options{
 			Environment: "development",
 			Port:        "3005",
-		})
+		}
+		app = service.New(options)
 
 		apiSubRouter := app.Router.PathPrefix("/api").Subrouter()
 		auth := middleware.AuthenticationMW{SystemToken: "blah"}
@@ -30,9 +29,10 @@ func App() *service.Rebar {
 		apiSubRouter.Use(middleware.Logger)
 		apiSubRouter.HandleFunc("/status", api.Status())
 
-		app.Router.HandleFunc("/query", buildGraphQLHandler())
-		app.Router.HandleFunc("/play", playground.Handler("Test", "/query"))
-
+		app.Router.HandleFunc("/query", buildGraphQLHandler(options.Environment))
+		if options.Environment == "development" {
+			app.Router.HandleFunc("/play", playground.Handler("Playground", "/query"))
+		}
 	}
 	return app
 }
@@ -41,31 +41,24 @@ type handlerInterface interface {
 	ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
-func buildGraphQLHandler() func(w http.ResponseWriter, r *http.Request) {
+func buildGraphQLHandler(environment string) func(w http.ResponseWriter, r *http.Request) {
 	c := cors.New(cors.Options{
-		/// is this going to work?
-		// AllowedOrigins: []string{"https://*.masonhub.co"},
 		AllowedOrigins: []string{"*"},
-		// AllowCredentials: true,
 		AllowedMethods: []string{http.MethodGet, http.MethodPost, http.MethodDelete},
 		AllowedHeaders: []string{"Content-Type", "Accept-Language"},
-		Debug:          true,
+		Debug:          false,
 	})
-
-	/// Are iframes different headers? or host headers
-	gqlSrv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-	gqlSrv.AddTransport(transport.Options{})
-	gqlSrv.AddTransport(transport.GET{})
-	gqlSrv.AddTransport(transport.POST{})
-	gqlSrv.AddTransport(transport.MultipartForm{})
-	gqlSrv.AddTransport(transport.Websocket{ // is that needed?
-		KeepAlivePingInterval: 10 * time.Second,
-		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		},
-	})
+	var gqlSrv *handler.Server
+	if environment == "development" {
+		//GraphQL playground schema does not load with `New` but works with `NewDefaultServer`
+		gqlSrv = handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	} else {
+		gqlSrv = handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+		gqlSrv.AddTransport(transport.Options{})
+		gqlSrv.AddTransport(transport.GET{})
+		gqlSrv.AddTransport(transport.POST{})
+		gqlSrv.AddTransport(transport.MultipartForm{})
+	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		c.Handler(gqlSrv).ServeHTTP(w, r)
